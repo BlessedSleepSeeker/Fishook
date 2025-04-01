a11f368f53da0384ad19117a575e2abc3005732f
Camille Gouneau <27480916+BlessedSleepSeeker@users.noreply.github.com>
2025-04-01 11:12:21 +0200

    Fixed instant DJ with controller + move input

--- fishook/character/state_machine/states/jump.gd
+++ fishook/character/state_machine/states/jump.gd
@@ -1,18 +1,23 @@
 extends CharacterState
 class_name JumpState
 
+@export var dj_minimum_delay: int = 6
+
 @onready var rdm_stream_player: RandomStreamPlayer = $RandomStreamPlayer
+
+var current_frame: int = 0
 
 func enter(_msg := {}) -> void:
 	character.velocity.y += physics_parameters.JUMP_IMPULSE
 	rdm_stream_player.play_random()
 	fade_crosshair(true)
+	current_frame = 0
 	super()
 
 func unhandled_input(_event: InputEvent):
 	super(_event)
 	print(_event.as_text())
-	if Input.is_action_just_pressed("jump") && not character.did_double_jump:
+	if Input.is_action_just_pressed("jump") && not character.did_double_jump && current_frame > dj_minimum_delay:
 		state_machine.transition_to("DoubleJump")
 	if Input.is_action_just_pressed("throw_hook"):
 		state_machine.transition_to("HookAiming")
@@ -24,3 +29,4 @@ func physics_update(_delta: float, _move_character: bool = true) -> void:
 		state_machine.transition_to("Land")
 	if character.velocity.y < 0:
 		state_machine.transition_to("Fall")
+	current_frame += 1
